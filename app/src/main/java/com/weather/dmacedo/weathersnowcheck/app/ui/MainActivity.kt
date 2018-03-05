71d6133dd7ff57fad5a90586a1e5d1837b0014e8
Daniel Macedo <dmacedo@ciandt.com>
2018-03-05 16:20:32 -0300

    First Commit

---
+++ app/src/main/java/com/weather/dmacedo/weathersnowcheck/app/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.weather.dmacedo.weathersnowcheck.app.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.weather.dmacedo.weathersnowcheck.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
